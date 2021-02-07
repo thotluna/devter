209994450655bc846f16b7b979780fbfd521c271
thotluna <eladi.feijoo@gmail.com>
2021-02-06 20:35:30 -0400

    separated styles of footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,6 +1,8 @@
 import GitHub from "components/Icons/GitHub"
 import Logo from "components/Icons/Logo"
 
+import styles from "components/Footer/styles"
+
 import { colors } from "styles/theme"
 
 export default function Footer() {
@@ -10,20 +12,7 @@ export default function Footer() {
         <GitHub fill={colors.primary} width={24} />
         <Logo fill={colors.primary} width={24} />
       </div>
-      <style jsx>{`
-        div {
-          position: sticky;
-          top: 0;
-          width: 100%;
-          height: 49px;
-          padding: 0.5rem 1rem;
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-
-          border-top: 1px solid #ccc;
-        }
-      `}</style>
+      <style jsx>{styles}</style>
     </>
   )
 }
---
+++ components/Footer/styles.js
@@ -0,0 +1,15 @@
+import css from "styled-jsx/css"
+
+export default css`
+  div {
+    position: sticky;
+    top: 0;
+    width: 100%;
+    height: 49px;
+    padding: 0.5rem 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-top: 1px solid #ccc;
+  }
+`
