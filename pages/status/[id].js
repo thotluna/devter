a9b7e57bc4fde28579645b856d17df1878fee006
thotluna <eladi.feijoo@gmail.com>
2021-02-10 14:59:38 -0400

    fetching data with getServerSideProps

--- pages/status/[id].js
+++ pages/status/[id].js
@@ -1,20 +1,33 @@
 import Devit from "components/Devit"
 
 export default function DevitPage(props) {
-  console.log("id", props)
   return <Devit {...props} />
 }
 
-DevitPage.getInitialProps = (context) => {
-  const { query, res } = context
-  const { id } = query
+export async function getServerSideProps(content) {
+  const { params, res } = content
+  const { id } = params
 
-  return fetch(`http://localhost:3000/api/devit/${id}`).then((apiRes) => {
-    if (apiRes.ok) {
-      return apiRes.json()
-    }
-    if (res) {
-      res.writeHead(301, { location: "/home" }).end()
-    }
-  })
+  const apiRes = await fetch(`http://localhost:3000/api/devit/${id}`)
+  if (apiRes.ok) {
+    const props = await apiRes.json()
+    return { props }
+  }
+  if (res) {
+    res.writeHead(301, { location: "/home" }).end()
+  }
 }
+
+// DevitPage.getInitialProps = (context) => {
+//   const { query, res } = context
+//   const { id } = query
+
+//   return fetch(`http://localhost:3000/api/devit/${id}`).then((apiRes) => {
+//     if (apiRes.ok) {
+//       return apiRes.json()
+//     }
+//     if (res) {
+//       res.writeHead(301, { location: "/home" }).end()
+//     }
+//   })
+// }
