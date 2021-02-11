449cf2d2ff3badecd3a89f735e8118191886c541
thotluna <eladi.feijoo@gmail.com>
2021-02-11 15:18:13 -0400

    featching data

--- hooks/useTimeAgo.js
+++ hooks/useTimeAgo.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+
 const DATE_UNITS = [
   ["day", 86400],
   ["hour", 3600],
@@ -9,19 +11,57 @@ const getDateDiffs = (timestamp) => {
   const now = Date.now()
   const elapsed = (timestamp - now) / 1000
 
-  for (const [unit, secondInUnit] of DATE_UNITS) {
-    if (Math.abs(elapsed) > secondInUnit || unit === "second") {
-      const value = Math.floor(elapsed / secondInUnit)
+  for (const [unit, secondsInUnit] of DATE_UNITS) {
+    if (Math.abs(elapsed) > secondsInUnit || unit === "second") {
+      const value = Math.round(elapsed / secondsInUnit)
       return { value, unit }
     }
   }
 }
 
 export default function useTimeAgo(timestamp) {
-  const { value, unit } = getDateDiffs(timestamp)
-  const rft = new Intl.RelativeTimeFormat("es", {
-    style: "short",
-  })
+  const [timeago, setTimeago] = useState(() => getDateDiffs(timestamp))
 
-  return rft.format(value, unit)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newTimeAgo = getDateDiffs(timestamp)
+      setTimeago(newTimeAgo)
+    }, 5000)
+
+    return () => clearInterval(interval)
+  }, [timestamp])
+
+  const rtf = new Intl.RelativeTimeFormat("es", { style: "short" })
+
+  const { value, unit } = timeago
+
+  return rtf.format(value, unit)
 }
+
+// const DATE_UNITS = [
+//   ["day", 86400],
+//   ["hour", 3600],
+//   ["minute", 60],
+//   ["second", 1],
+// ]
+
+// const getDateDiffs = (timestamp) => {
+//   const now = Date.now()
+//   const elapsed = (timestamp - now) / 1000
+
+//   for (const [unit, secondInUnit] of DATE_UNITS) {
+//     if (Math.abs(elapsed) > secondInUnit || unit === "second") {
+//       const value = Math.floor(elapsed / secondInUnit)
+//       return { value, unit }
+//     }
+//   }
+// }
+
+// export default function useTimeAgo(timestamp) {
+//   const { value, unit } = getDateDiffs(timestamp)
+//   const rft = new Intl.RelativeTimeFormat("es", {
+//     style: "short",
+//   })
+
+//   return rft.format(value, unit)
+// }
--- pages/status/[id].js
+++ pages/status/[id].js
@@ -1,22 +1,71 @@
 import Devit from "components/Devit"
+import { firestore } from "firebase/admin"
+import { useRouter } from "next/router"
 
 export default function DevitPage(props) {
+  const router = useRouter()
+  console.log("loading", router.isFallback)
+  if (router.isFallback) {
+    console.log("loading", router.isFallback)
+    return "Loading ..."
+  }
   return <Devit {...props} />
 }
 
-export async function getServerSideProps(content) {
-  const { params, res } = content
-  const { id } = params
-
-  const apiRes = await fetch(`http://localhost:3000/api/devit/${id}`)
-  if (apiRes.ok) {
-    const props = await apiRes.json()
-    return { props }
-  }
-  if (res) {
-    res.writeHead(301, { location: "/home" }).end()
+export async function getStaticPaths() {
+  return {
+    // paths: [{ params: { id: "2qxnnAWLNGCQ4lvC90Ri" } }],
+    paths: [],
+    fallback: true,
   }
 }
+
+export async function getStaticProps(content) {
+  const { params /*, res */ } = content
+  const { id } = params
+
+  return firestore
+    .collection("devits")
+    .doc(id)
+    .get()
+    .then((doc) => {
+      const data = doc.data()
+      const id = doc.id
+      const { createdAt } = data
+
+      const props = {
+        ...data,
+        id,
+        createdAt: +createdAt.toDate(),
+      }
+      return { props }
+    })
+    .catch(() => {
+      return { props: {} }
+    })
+
+  // const apiRes = await fetch(`http://localhost:3000/api/devit/${id}`)
+  // if (apiRes.ok) {
+  //   const props = await apiRes.json()
+  //   return { props }
+  // }
+  // if (res) {
+  //   res.writeHead(301, { location: "/home" }).end()
+  // }
+}
+// export async function getServerSideProps(content) {
+//   const { params, res } = content
+//   const { id } = params
+
+//   const apiRes = await fetch(`http://localhost:3000/api/devit/${id}`)
+//   if (apiRes.ok) {
+//     const props = await apiRes.json()
+//     return { props }
+//   }
+//   if (res) {
+//     res.writeHead(301, { location: "/home" }).end()
+//   }
+// }
 
 // DevitPage.getInitialProps = (context) => {
 //   const { query, res } = context
